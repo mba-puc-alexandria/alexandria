3cc9c4895b6ef8ba042ece700b7c15996f58713e
Carla Talita Alves dos Santos <caarlatalita1@hotmail.com>
2026-05-01 16:23:27 -0300

    feat: Adjusting User and Book attributes (#3)

--- alexandria-backend/src/main/java/com/pucsp/alexandria/adapter/out/persistence/entity/UserEntity.java
+++ alexandria-backend/src/main/java/com/pucsp/alexandria/adapter/out/persistence/entity/UserEntity.java
@@ -1,20 +1,15 @@
 package com.pucsp.alexandria.adapter.out.persistence.entity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "users")
-
 public class UserEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,6 +17,12 @@ public class UserEntity {
 
   @Column(unique = true, nullable = false)
   private String username;
+
+  @Column(name = "first_name", nullable = false)
+  private String firstName;
+
+  @Column(name = "last_name", nullable = false)
+  private String lastName;
 
   @Column(unique = true, nullable = false)
   private String email;
@@ -32,6 +33,67 @@ public class UserEntity {
   @Column(name = "created_at", updatable = false)
   private LocalDateTime createdAt = LocalDateTime.now();
 
-  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-  private List<UserBooksEntity> library = new ArrayList<>();
+  public UserEntity() {}
+
+  public UserEntity(Long id, String username, String firstName, String lastName, String email, String password, LocalDateTime createdAt) {
+    this.id = id;
+    this.username = username;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+    this.createdAt = createdAt;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
 }
