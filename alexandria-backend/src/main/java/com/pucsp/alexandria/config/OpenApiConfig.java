4ac8aa1a930dd0c7d79bb627a84647c52c71f4a7
Carla Talita <caarlatalita1@hotmail.com>
2026-05-22 23:54:27 -0300

    plano para autenticação seletiva -atualizando documentação backend

--- alexandria-backend/src/main/java/com/pucsp/alexandria/config/OpenApiConfig.java
+++ alexandria-backend/src/main/java/com/pucsp/alexandria/config/OpenApiConfig.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +15,7 @@ public class OpenApiConfig {
   public OpenAPI customOpenAPI() {
     final String securitySchemeName = "bearerAuth";
 
-    return new OpenAPI()
+        return new OpenAPI()
         .info(new Info()
             .title("Alexandria API")
             .version("0.0.1-SNAPSHOT")
@@ -24,11 +23,8 @@ public class OpenApiConfig {
             .license(new License()
                 .name("Apache 2.0")
                 .url("https://www.apache.org/licenses/LICENSE-2.0")))
-        .addSecurityItem(new SecurityRequirement()
-            .addList(securitySchemeName))
         .components(new Components()
             .addSecuritySchemes(securitySchemeName, new SecurityScheme()
-                .name(securitySchemeName)
                 .type(SecurityScheme.Type.HTTP)
                 .scheme("bearer")
                 .bearerFormat("JWT")));
