8849faa9915805d2350238211288d1c5d0cb19dd
Ronaldo Luiz <ronaldoluiz747@gmail.com>
2026-06-10 17:44:26 -0300

    fix: hardcoda client_id do Google no provider do frontend

--- alexandria-frontend/src/components/GoogleProvider.tsx
+++ alexandria-frontend/src/components/GoogleProvider.tsx
@@ -5,7 +5,7 @@ import { ReactNode } from 'react';
 
 export default function GoogleProvider({ children }: { children: ReactNode }) {
   return (
-    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+    <GoogleOAuthProvider clientId="585326352858-p23r7po387fmga55vbv5hjqf26ah35db.apps.googleusercontent.com">
       {children}
     </GoogleOAuthProvider>
   );
