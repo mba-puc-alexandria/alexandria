a79e71e63edcfe8130735e6606f5ca278ccdcc03
Tamiriscoliveira <tami_coliveira@hotmail.com>
2026-08-16 16:23:08 -0300

    configuração nome do projeto no iac

--- iac/ecr.tf
+++ iac/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "app" {
-  name                 = "linuxtips/sorteador-strigus"
+  name                 = "mba-puc-alexandria/alexandria"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
